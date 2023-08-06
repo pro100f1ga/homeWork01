5b5cffd877544fefc6e9ca9b548deac3aad04304
Dmitriy <140834910+pro100f1ga@users.noreply.github.com>
2023-08-06 10:56:45 +0300

    обновил 4ое задание

--- zadanie004/Program.cs
+++ zadanie004/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Clear();
+int evenNumber =2;
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+if (num1 > 1)
+{
+    while(evenNumber<=num1)
+    {
+        Console.Write(evenNumber+",");
+        evenNumber = evenNumber+2;
+    }
+}
